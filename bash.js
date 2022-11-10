600f30fc6b38bbf531f792e008ba9fe3b3895cea
Steven King <king.steven.t@gmail.com>
2022-11-10 10:35:15 -0700

    Global Process Module Completed

---
+++ bash.js
@@ -0,0 +1,13 @@
+//Output a prompt
+
+process.stdout.write('prompt > ');
+const { cwd } = require('node:process');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); //remove the newline
+  if(cmd === 'pwd') {
+    console.log(`${cwd()}`);
+  }
+});
+process.stdout.write('prompt > ');
